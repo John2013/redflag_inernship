c2563c081196ace777b24b907b5b49f63a79f490
Evgeny Timofeyev <nawasington@ya.ru>
2018-06-07 14:44:07 +0300

    js vars 3

--- 3_js/1_vars/3/script.js
+++ 3_js/1_vars/3/script.js
@@ -1,2 +1,13 @@
-const number = 8;
+const number = 15;
 
+const bin_number = +(number.toString(2));
+
+let ones_count =
+	Math.floor(bin_number/1000) +
+	Math.floor((bin_number%1000)/100) +
+	Math.floor((bin_number%100)/10) +
+	Math.floor((bin_number%10));
+
+
+
+console.log(ones_count);
