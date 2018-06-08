0b0ce96dcac5cfc0740ca51c581be7129ad5d0c2
Evgeny Timofeyev <nawasington@ya.ru>
2018-06-08 15:08:39 +0300

    js loops 9

---
+++ 3_js/4_loops/9/script.js
@@ -0,0 +1,15 @@
+const app = document.getElementById('app');
+const n = 10;
+
+const get_range = (start, stop, step = 1) => Array(stop - start).fill(start).map((x, y) => x + y * step);
+
+const get_random_int = (min, max) => Math.floor(Math.random() * (max - min + 1)) + min;
+
+const get_random_array =
+	(n, range_array) => Array(n).fill(0).map(() => range_array[get_random_int(0, range_array.length - 1)]);
+
+const array = get_random_array(n, get_range(-20, 25));
+
+const min = Math.min(...array);
+
+app.innerText = `${JSON.stringify(array)}\nномер наименьшего числа: ${array.indexOf(min) + 1}`;
