99a2b427ba9d31aa161f4e30068fe8852801c06a
Evgeny Timofeyev <nawasington@ya.ru>
2018-06-08 17:32:53 +0300

    js loops 8

--- 3_js/4_loops/8/script.js
+++ 3_js/4_loops/8/script.js
@@ -1,17 +1,28 @@
 const app = document.getElementById('app');
+const number1 = +prompt('Введи натуральное число');
+const number2 = +prompt('Введи второе натуральное число');
+
+const get_dividers_array = (number) => {
+	let dividers_arr = [1],
+		checking_number = 2;
+
+	while (checking_number < number) {
+		if(number % checking_number === 0){
+			dividers_arr.push(checking_number)
+		}
+		checking_number += 1
+	}
+	return dividers_arr
+};
+
+const sum = (array)=>array.reduce((sum_, item)=>sum_+item);
+
+const dividers_array_1 = get_dividers_array(number1);
+const dividers_array_2 = get_dividers_array(number2);
+
+const is_friendly = ((sum(dividers_array_1) === number2) && sum(dividers_array_2) === number1);
 
 
-const button = document.getElementById('button');
-
-
-button.addEventListener('click', () => {
-	const array = document.getElementById('array')
-		.value
-		.trim()
-		.split('\n')
-		.map((item)=>+item);
-
-	const min = Math.min(...array);
-
-	app.innerText = `${min}`
-});
+app.innerText = `${JSON.stringify(dividers_array_1)} = ${sum(dividers_array_1)}
+${JSON.stringify(dividers_array_2)} = ${sum(dividers_array_2)}
+Числа дружественные - ${is_friendly}`;
