36eb7612a428da3c859298f59fca16de235dc183
Evgeny Timofeyev <nawasington@ya.ru>
2018-06-08 11:03:59 +0300

    js arr 6

--- 3_js/3_arrays/6/script.js
+++ 3_js/3_arrays/6/script.js
@@ -1,16 +1,21 @@
 const n = 10;
 
-const get_range = (start, stop, step=1) => Array(stop - start).fill(start).map((x, y) => x + y * step);
+const get_range = (start, stop, step = 1) => Array(stop - start).fill(start).map((x, y) => x + y * step);
 
 const get_random_int = (min, max) => Math.floor(Math.random() * (max - min + 1)) + min;
 
 const range = get_range(-19, 26);
 
-let result_array = Array(n).fill(0).map(()=>range[get_random_int(0, range.length - 1)]);
+let result_array = Array(n).fill(0).map(() => range[get_random_int(0, range.length - 1)]);
 
-console.log(result_array.reduce((sum, item) => {
-	if (item % 2 === 1){
-		return sum + item
-	}
-	return sum
-}));
+console.log(
+	result_array.reduce(
+		(sum, item) => {
+			if (item % 2 === 1) {
+				return sum + item
+			}
+			return sum
+		},
+		0
+	)
+);
