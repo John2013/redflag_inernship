63a40cee8ea5255c7dba6301a76811efc77adddd
Evgeny Timofeyev <nawasington@ya.ru>
2018-06-08 18:10:22 +0300

    js loops 10

--- 3_js/4_loops/10/script.js
+++ 3_js/4_loops/10/script.js
@@ -1,9 +1,19 @@
 const app = document.getElementById('app');
-const array = [4, 0, 0, -13, 255, -0];
+let number1 = +prompt('Введи натуральное число');
+let number2 = +prompt('Введи второе натуральное число');
 
+const is_divider = (number, divider) => number % divider === 0;
 
-app.innerText = `${JSON.stringify(array)}\nколичество нулей: ${array.reduce((cnt_zeros, item) => {
-	if (item === 0)
-		return cnt_zeros + 1;
-	return cnt_zeros
-}, 0)}`;
+if (number1 > number2){
+	const tmp = number2;
+	number2 = number1;
+	number1 = tmp
+}
+
+let nok = number2 + 1;
+
+while(!(is_divider(nok,number1) && is_divider(nok,number2))){
+	nok += 1
+}
+
+app.innerText =`НОК(${number1},${number2}) = ${nok}`;
