c6703487823ec9cd2373de3a3f92c06bfc4451e5
Evgeny Timofeyev <nawasington@ya.ru>
2018-06-08 15:55:19 +0300

    js loops 1

--- 3_js/4_loops/1/script.js
+++ 3_js/4_loops/1/script.js
@@ -1,15 +1,4 @@
 const app = document.getElementById('app');
-const n = 10;
-
-const get_range = (start, stop, step = 1) => Array(stop - start).fill(start).map((x, y) => x + y * step);
-
-const get_random_int = (min, max) => Math.floor(Math.random() * (max - min + 1)) + min;
-
-const range = get_range(-23, 34);
-
-const get_random_array =
-	(n, range_array) => Array(n).fill(0).map(() => range_array[get_random_int(0, range_array.length - 1)]);
-
-let result_array = get_random_array(n, range);
-
-app.innerText = JSON.stringify(result_array);
+const number = +prompt('Введи натуральное число');
+const arr = Array(number).fill(0).map((item, index)=> index + 1);
+app.innerText = `${JSON.stringify(arr)}`;
