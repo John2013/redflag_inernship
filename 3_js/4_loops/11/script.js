ee9fba3817cde6b827ddb7cdfe291da7b4ff2ee9
Evgeny Timofeyev <nawasington@ya.ru>
2018-06-09 11:15:38 +0300

    js loops 11

--- 3_js/4_loops/11/script.js
+++ 3_js/4_loops/11/script.js
@@ -5,10 +5,21 @@ const n = +prompt('Введи n');
 // app.innerHTML = `x<sup>2</sup> = ${x ** n}`;
 // это было бы слишком просто, да?
 
-let result = x;
 
-for (let i = 1; i < n; i++){
-	result *= x;
-}
+const simple_pow = (x, n=2) => {
+	let result = x;
+	if(n === 0){
+		return 1;
+	}
+	if(n === 1){
+		return x;
+	}
+	for (let i = 1; i < n; i++){
+		result *= x;
+	}
+	return result
+};
 
-app.innerHTML = `x<sup>2</sup> = ${result}`;
+
+
+app.innerHTML = `${x}<sup>${n}</sup> = ${simple_pow(x, n)}`;
