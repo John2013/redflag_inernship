86c4fa00b46e5c85adcdfafa1b6bcd034df298db
Evgeny Timofeyev <nawasington@ya.ru>
2018-06-09 12:42:18 +0300

    js loops 14 начат

--- 3_js/3_arrays/15/script.js
+++ 3_js/3_arrays/15/script.js
@@ -1,5 +1,5 @@
-const array_A = [4, 5, 1, -13, 255, -1];
+const array_A = [4, 5, 1, -13, 255, -1, 78, 99];
 
-const array_B = [array_A[1], array_A[3], array_A[5]];
+const array_B = [array_A[1], array_A[3], array_A[5], array_A[7]];
 
 console.log(array_B);
--- 3_js/4_loops/14/script.js
+++ 3_js/4_loops/14/script.js
@@ -1,1 +1,9 @@
 const app = document.getElementById('app');
+const button = document.getElementById('button');
+
+button.addEventListener('click', ()=>{
+	const x = +document.getElementById('x').value;
+	const a = document.getElementById('a').value.trim().split('\n').map((item)=>+item)
+
+
+});
