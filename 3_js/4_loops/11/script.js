7aafe1cc2e450f305c4d896c435a4fcebd940651
Evgeny Timofeyev <nawasington@ya.ru>
2018-06-09 10:32:52 +0300

    js loops 11

--- 3_js/4_loops/11/script.js
+++ 3_js/4_loops/11/script.js
@@ -2,4 +2,13 @@ const app = document.getElementById('app');
 const x = +prompt('Введи x');
 const n = +prompt('Введи n');
 
-app.innerHTML = `x<sup>2</sup> = ${x ** n}`;
+// app.innerHTML = `x<sup>2</sup> = ${x ** n}`;
+// это было бы слишком просто, да?
+
+let result = x;
+
+for (let i = 1; i < n; i++){
+	result *= x;
+}
+
+app.innerHTML = `x<sup>2</sup> = ${result}`;
