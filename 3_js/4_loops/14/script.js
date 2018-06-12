aa4cd633bb55ed04ca8f91e48a70e9a8a6296f3e
Evgeny Timofeyev <nawasington@ya.ru>
2018-06-12 12:14:56 +0300

    js loops 14

--- 3_js/4_loops/14/script.js
+++ 3_js/4_loops/14/script.js
@@ -3,7 +3,9 @@ const button = document.getElementById('button');
 
 button.addEventListener('click', ()=>{
 	const x = +document.getElementById('x').value;
-	const a = document.getElementById('a').value.trim().split('\n').map((item)=>+item)
+	const a = document.getElementById('a').value.trim().split('\n').map((item)=>+item);
 
-
+	const polynomial = a.reduce((polynomial, item, index)=>item * x ** index);
+	app.innerText = `значение многочлена степени ${a.length} с коэффициентами ${a.join(', ')} от одной переменной в ` +
+		`точке ${x} - ${polynomial}`
 });
