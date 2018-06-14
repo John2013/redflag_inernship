3134b6de0d148cabd7034784d04466b1291f546e
Evgeny Timofeyev <nawasington@ya.ru>
2018-06-14 12:09:38 +0300

    js loops 39

---
+++ 3_js/4_loops/39/script.js
@@ -0,0 +1,29 @@
+const app = document.getElementById('app');
+const button = document.getElementById('button');
+
+function get_factorial(n) {
+	let factorial = 1;
+
+	for (let i = 1; i <= n; i += 1){
+		factorial *= i
+	}
+
+	return factorial
+}
+
+const eInX = (x, eps = 100) => {
+	let sum = 1;
+
+	for (let i = 1; i <= eps; i += 1) {
+		sum += x ** i / get_factorial(i)
+	}
+
+	return sum
+};
+
+
+button.addEventListener('click', () => {
+	const x = +document.getElementById('x').value;
+
+	app.innerHTML = `e<sup>x</sup> = ${eInX(x)}`
+});
