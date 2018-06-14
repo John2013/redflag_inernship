aa6bc6a55ba529d3a86394fa7ac96530163bf956
Evgeny Timofeyev <nawasington@ya.ru>
2018-06-14 11:03:22 +0300

    js loops 36

---
+++ 3_js/4_loops/36/script.js
@@ -0,0 +1,31 @@
+const app = document.getElementById('app');
+const button = document.getElementById('button');
+
+const getFibonacciSum = (n) => {
+	if(n === 1) return 0;
+	if(n === 2) return 1;
+
+	let f_minus_2 = 0;
+	let f_minus_1 = 1;
+	let f = 0;
+
+	let sum = 0;
+
+	for (let i = 2; i <= n; i += 1){
+		f = f_minus_2 + f_minus_1;
+
+		f_minus_2 = f_minus_1;
+		f_minus_1 = f;
+
+		sum += f
+	}
+
+	return sum
+};
+
+
+button.addEventListener('click', () => {
+	const n = +document.getElementById('n').value;
+
+	app.innerText = `${getFibonacciSum(n)}`
+});
