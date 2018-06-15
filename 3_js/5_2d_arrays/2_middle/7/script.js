59a78c9cd28dc8ea4289b3bf66296fd0bce2a1fc
Evgeny Timofeyev <nawasington@ya.ru>
2018-06-15 16:14:33 +0300

    js 2d_arrays middle 7

--- 3_js/5_2d_arrays/2_middle/7/script.js
+++ 3_js/5_2d_arrays/2_middle/7/script.js
@@ -4,12 +4,13 @@ const button = getById('button');
 
 button.addEventListener('click', () => {
 	const n = getNumById('n');
+	const row = getNumById('row');
 
 	const array = randIntMatrix(n, n, 0, 9);
 
 	const tableArray = getTable(array);
 
-	app.innerHTML = `${tableArray}`
+	app.innerHTML = `${tableArray}<br>Сумма строки ${row}: ${array[row].reduce((s, v) => s + v)}`
 });
 
 
