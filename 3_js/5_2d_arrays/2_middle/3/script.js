0b501d2112c39347d97537ec3b02e8c277bb7cc5
Evgeny Timofeyev <nawasington@ya.ru>
2018-06-15 14:02:12 +0300

    js 2d_arrays middle 3

--- 3_js/5_2d_arrays/2_middle/3/script.js
+++ 3_js/5_2d_arrays/2_middle/3/script.js
@@ -1,6 +1,20 @@
 const app = getById('app');
 const button = getById('button');
 
+const getMaxSumRow = (matrix) =>{
+	const sumArray = matrix.map((i)=>i.reduce((sum, value)=> sum + value));
+	return sumArray.reduce(
+		(maxRowSumIndex, rowSum, index, thisArray)=>{
+			if(rowSum > thisArray[maxRowSumIndex])
+				return index;
+
+			return maxRowSumIndex
+		},
+		0
+	);
+};
+
+
 
 button.addEventListener('click', () => {
 	const n = getNumById('n');
@@ -9,7 +23,9 @@ button.addEventListener('click', () => {
 
 	const tableArray = getTable(array);
 
-	app.innerHTML = `${tableArray}`
+	const maxRowIndex = getMaxSumRow(array);
+
+	app.innerHTML = `${tableArray}<br>Номер максимальной строки: ${maxRowIndex}; сумма: ${array[maxRowIndex].reduce((s,v)=>s+v)}`
 });
 
 
