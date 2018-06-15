094196f9b464f137e89370f4f8e88d7cf79b13e4
Evgeny Timofeyev <nawasington@ya.ru>
2018-06-15 13:33:59 +0300

    js 2d_arrays middle 2

--- 3_js/5_2d_arrays/2_middle/2/script.js
+++ 3_js/5_2d_arrays/2_middle/2/script.js
@@ -1,15 +1,23 @@
 const app = getById('app');
 const button = getById('button');
 
+const negativeDiagCount = (matrix, isMainDiag = true) =>
+	matrixDiagReduce(
+		matrix,
+		(count, value) => count + +(value < 0),
+		0,
+		isMainDiag
+	);
 
 button.addEventListener('click', () => {
 	const n = getNumById('n');
 
-	const array = randIntMatrix(n, n, 0, 9);
+	const array = randIntMatrix(n, n, -1, 1);
 
 	const tableArray = getTable(array);
 
-	app.innerHTML = `${tableArray}`
+	app.innerHTML = `${tableArray}<br>Отрицательных по диагоналям: ${negativeDiagCount(array, true)}; ` +
+		`${negativeDiagCount(array, false)}`
 });
 
 
