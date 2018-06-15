818c08d90c9fe5231c5cb12dbb25769044bfe463
Evgeny Timofeyev <nawasington@ya.ru>
2018-06-15 11:12:05 +0300

    js 2d_arrays simple 5

--- 3_js/5_2d_arrays/1_simple/5/script.js
+++ 3_js/5_2d_arrays/1_simple/5/script.js
@@ -1,29 +1,44 @@
 const app = getById('app');
 const button = getById('button');
 
-const getMaxByCol = (array) =>
-	array.reduce(
-		(maxArray, item) => {
+/**
+ * Вызывает заданную функцию для элементов на главной диагонали матрицы и аккумулирует результаты функции
+ * @param matrix двумерный массив n x n
+ * @param callbackFn(prevValue, currentValue, index, matrix) функция, вызывающаяся для элементов диагонали
+ * @param initialValue начальное значение, если не задано, то берётся первый элемент диагонали, а цикл начинается со
+ * второго элемента
+ */
+const matrixDiagReduce = (matrix, callbackFn, initialValue = null) => {
 
-			item.forEach((value, index) =>
-				maxArray[index] = Math.max(maxArray[index], value));
+	let startIndex = 0;
+	if (initialValue === null){
+		initialValue = matrix[0][0];
+		startIndex = 1;
+	}
 
-			return maxArray
+	let prevValue = initialValue;
 
-		},
-		Array(array[0].length).fill(0)
-	);
+	for (let i = startIndex; i < matrix.length; i += 1) {
+		prevValue = callbackFn(prevValue, matrix[i][i], i, matrix)
+	}
+
+	return prevValue
+};
+
+const getMatrixPositivesItemsCountOnDiagonal = (matrix) => {
+	const positivesCount = matrixDiagReduce(matrix, (count, value) => count + +(value > 0))
+};
 
 button.addEventListener('click', () => {
 	const n = getNumById('n');
 
-	const array = randIntMatrix(n, n, 0, 255);
+	const matrix = randIntMatrix(n, n, 0, 255);
 
-	const tableArray = getTable(array);
+	const tableArray = getTable(matrix);
 
-	const maxByColArray = getMaxByCol(array);
+	const diagPositivesCount = getMatrixPositivesItemsCountOnDiagonal(matrix);
 
-	app.innerHTML = `${tableArray}<br>максимум по колонкам: ${maxByColArray.join(', ')}`
+	app.innerHTML = `${tableArray}<br>Положительных на диагонали: ${diagPositivesCount}`
 });
 
 
