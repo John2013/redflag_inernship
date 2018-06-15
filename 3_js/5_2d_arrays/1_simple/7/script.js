f1c23ce6b3745d7203f7aa666c53ee78f83ed2c3
Evgeny Timofeyev <nawasington@ya.ru>
2018-06-15 12:50:17 +0300

    js 2d_arrays simple 7

--- 3_js/5_2d_arrays/1_simple/7/script.js
+++ 3_js/5_2d_arrays/1_simple/7/script.js
@@ -1,29 +1,24 @@
 const app = getById('app');
 const button = getById('button');
 
-const getMaxByCol = (array) =>
-	array.reduce(
-		(maxArray, item) => {
-
-			item.forEach((value, index) =>
-				maxArray[index] = Math.max(maxArray[index], value));
-
-			return maxArray
-
-		},
-		Array(array[0].length).fill(0)
+const matrixCountOfX = (array, x) =>
+	matrixReduce(
+		array,
+		(count, value) => count + +(value === x),
+		0
 	);
 
 button.addEventListener('click', () => {
-	const n = getNumById('n');
+	const n = getNumById('n'),
+		x = getNumById('x');
 
-	const array = randIntMatrix(n, n, 0, 255);
+	const array = randIntMatrix(n, n, 0, 9);
 
 	const tableArray = getTable(array);
 
-	const maxByColArray = getMaxByCol(array);
+	const count = matrixCountOfX(array, x);
 
-	app.innerHTML = `${tableArray}<br>максимум по колонкам: ${maxByColArray.join(', ')}`
+	app.innerHTML = `${tableArray}<br>Количество x: ${count}`
 });
 
 
