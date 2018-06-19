bf8266a563f8359fe539fb06069964af2c8a3610
Evgeny Timofeyev <nawasington@ya.ru>
2018-06-19 13:17:24 +0300

    js 2d_arrays middle 12

--- 3_js/5_2d_arrays/2_middle/12/script.js
+++ 3_js/5_2d_arrays/2_middle/12/script.js
@@ -1,15 +1,29 @@
 const app = getById('app');
 const button = getById('button');
 
+const sum = array => array.reduce((sum, item) => sum + item);
+
+const middleArithmetic = array => sum(array) / array.length;
+
+const getMiddlesArray = matrix => matrix.map(array => middleArithmetic(array));
+
+const getList = array => {
+	let list = '<ul>';
+
+	array.forEach(value => list += `<li>${value}</li>`);
+
+	return list + '</ul>'
+};
 
 button.addEventListener('click', () => {
 	const n = getNumById('n');
+	const m = getNumById('m');
 
-	const array = randIntMatrix(n, n, 0, 9);
+	const array = randIntMatrix(n, m, 0, 9);
 
 	const tableArray = getTable(array);
 
-	app.innerHTML = `${tableArray}`
+	app.innerHTML = `${tableArray}<br>${getList(getMiddlesArray(array))}`
 });
 
 
