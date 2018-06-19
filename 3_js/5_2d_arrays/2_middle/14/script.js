ed9d03c2d90ce3ae35c3707781a690265aae2c49
Evgeny Timofeyev <nawasington@ya.ru>
2018-06-19 15:14:28 +0300

    js 2d_arrays middle 14

--- 3_js/5_2d_arrays/2_middle/14/script.js
+++ 3_js/5_2d_arrays/2_middle/14/script.js
@@ -1,15 +1,31 @@
 const app = getById('app');
 const button = getById('button');
 
+const sum = array => array.reduce((sum, value) => sum + value);
+
+const changeLastOnSum = matrix => {
+	let newMatrix = matrix.slice();
+	for (let i = 1; i < newMatrix.length; i += 1){
+		newMatrix[i].pop();
+		newMatrix[i].push(sum(newMatrix[i - 1]))
+	}
+	return newMatrix
+};
+
 
 button.addEventListener('click', () => {
 	const n = getNumById('n');
+	const m = getNumById('m');
 
-	const array = randIntMatrix(n, n, 0, 9);
+	const matrix = randIntMatrix(n, m, 0, 9);
 
-	const tableArray = getTable(array);
+	const tableMatrix = getTable(matrix);
 
-	app.innerHTML = `${tableArray}`
+	const matrix2 = changeLastOnSum(matrix);
+
+	const tableMatrix2 = getTable(matrix2);
+
+	app.innerHTML = `${tableMatrix}<br>${tableMatrix2}`
 });
 
 
