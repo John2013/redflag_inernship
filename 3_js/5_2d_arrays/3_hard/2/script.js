3de104814816463adb7114bd3460f31a7c2bcced
Evgeny Timofeyev <nawasington@ya.ru>
2018-06-20 11:36:32 +0300

    js 2d_arrays hard 2

--- 3_js/5_2d_arrays/3_hard/2/script.js
+++ 3_js/5_2d_arrays/3_hard/2/script.js
@@ -1,7 +1,18 @@
 const app = getById('app');
 const button = getById('button');
 
+const multiplyNon0MatrixDiagItems = matrix =>
+	matrixDiagReduce(matrix, (product, value) => product * (value || 1), null, true, true);
+
 
 button.addEventListener('click', () => {
+	const n = getNumById('n');
 
+	const matrix = randIntMatrix(n, n, 0, 9);
+
+	const matrixTable = getTable(matrix);
+
+	const diagProduct = multiplyNon0MatrixDiagItems(matrix);
+
+	app.innerHTML = `${matrixTable}<br>Произведение диагональных элементов: ${diagProduct}`
 });
--- 3_js/5_2d_arrays/base.js
+++ 3_js/5_2d_arrays/base.js
@@ -8,21 +8,21 @@ const randInt = (min, max) => Math.floor(randFloat(min, max));
 
 const randOfArray = array => array[randInt(0, array.length - 1)];
 
-const randChar = (chars='qwertyuiopasdfghjklzxcvbnm') => randOfArray(chars.split(''));
+const randChar = (chars = 'qwertyuiopasdfghjklzxcvbnm') => randOfArray(chars.split(''));
 
 const randIntArray = (n, min, max) => Array(n).fill(0).map(() => randInt(min, max));
 
 const randFloatArray = (n, min, max, fractionDigits = 5) => Array(n).fill(0).map(() =>
 	randFloat(min, max, fractionDigits));
 
-const randCharArray = (size, chars='qwertyuiopasdfghjklzxcvbnm') => Array(size).fill('').map(() => randChar(chars));
+const randCharArray = (size, chars = 'qwertyuiopasdfghjklzxcvbnm') => Array(size).fill('').map(() => randChar(chars));
 
 const randIntMatrix = (n, m, min, max) => Array(n).fill([]).map(() => randIntArray(m, min, max));
 
 const randFloatMatrix = (n, m, min, max, fractionDigits = 5) => Array(n).fill([]).map(() =>
 	randFloatArray(m, min, max, fractionDigits));
 
-const randCharMatrix = (n, m, chars='qwertyuiopasdfghjklzxcvbnm') =>
+const randCharMatrix = (n, m, chars = 'qwertyuiopasdfghjklzxcvbnm') =>
 	Array(n).fill([]).map(() =>
 		randCharArray(m, chars));
 
@@ -75,20 +75,23 @@ const matrixReduce = (matrix, callbackFn, initialValue = null) => {
 
 
 /**
- * Вызывает заданную функцию для элементов матрицы и аккумулирует результаты функции
+ * Вызывает заданную функцию для элементов матрицы и возвращает матрицу с результатами функции
  * @param matrix массив n x n
  * @param callbackFn(prevValue, currentValue, indexArray, matrix) функция, вызывающаяся для элементов диагонали
  */
 const matrixMap = (matrix, callbackFn) => {
 	let result = [];
-	for(let rowIndex in matrix){
-		for(let colIndex in matrix[rowIndex]){
-			result.push(callbackFn(matrix[rowIndex][colIndex],[rowIndex, colIndex], matrix))
+	for (let rowIndex in matrix) {
+		for (let colIndex in matrix[rowIndex]) {
+			result.push(callbackFn(matrix[rowIndex][colIndex], [rowIndex, colIndex], matrix))
 		}
 	}
 
 	return result
 };
+
+
+const isCenter = (i, size) => (size - 1) / 2 === i;
 
 
 /**
@@ -98,17 +101,25 @@ const matrixMap = (matrix, callbackFn) => {
  * @param initialValue начальное значение, если не задано, то берётся первый элемент диагонали, а цикл начинается со
  * второго элемента
  * @param isMainDiagonal boolean Главная или побочная диагональ
+ * @param isBoth boolean Обе диагонали (пересекающийся элемент просчитывается однажды)
  */
-const matrixDiagReduce = (matrix, callbackFn, initialValue = null, isMainDiagonal = true) => {
+const matrixDiagReduce = (matrix, callbackFn, initialValue = null, isMainDiagonal = true, isBoth = false) => {
 
 	let startIndex = 0;
 	if (initialValue === null) {
-		initialValue = matrix[0][0];
+		if (isMainDiagonal || isBoth)
+			initialValue = matrix[0][0];
+
+		else
+			initialValue = matrix[0][matrix[0].length - 1];
+
 		startIndex = 1;
 	}
 
 	let prevValue = initialValue;
 
+	if(isBoth)
+		isMainDiagonal = true;
 	let j;
 	for (let i = startIndex; i < matrix.length; i += 1) {
 		if (isMainDiagonal) {
@@ -117,6 +128,14 @@ const matrixDiagReduce = (matrix, callbackFn, initialValue = null, isMainDiagona
 			j = matrix.length - 1 - i
 		}
 		prevValue = callbackFn(prevValue, matrix[i][j], [i, j], matrix)
+	}
+
+	if(isBoth){
+		for (let i = startIndex; i < matrix.length; i += 1) {
+			j = matrix.length - 1 - i;
+			if (isCenter(j, matrix.length)) continue;
+			prevValue = callbackFn(prevValue, matrix[i][j], [i, j], matrix)
+		}
 	}
 
 	return prevValue
