91eb0dfb3ab71303fddf2919c56ae2cb08141c44
Evgeny Timofeyev <nawasington@ya.ru>
2018-06-20 14:28:17 +0300

    js 2d_arrays hard 5

--- 3_js/5_2d_arrays/3_hard/5/script.js
+++ 3_js/5_2d_arrays/3_hard/5/script.js
@@ -2,6 +2,29 @@ const app = getById('app');
 const button = getById('button');
 
 
+const revertDiagonals = matrix => {
+	let newMatrix = matrix.slice();
+	let j;
+	for (let i in matrix) {
+		j = matrix.length - 1 - i;
+		newMatrix[i][i] = matrix[j][j];
+		newMatrix[i][j] = matrix[j][i];
+	}
+	return newMatrix
+};
+
+
 button.addEventListener('click', () => {
 
+	const n = getNumById('n');
+
+	const matrix = randIntMatrix(n, n, 0, 9);
+
+	const matrixTable = getTable(matrix);
+
+	const revertDiagsMatrix = revertDiagonals(matrix);
+
+	const revertDiagsMatrixTable = getTable(revertDiagsMatrix);
+
+	app.innerHTML = `${matrixTable}<br>${revertDiagsMatrixTable}`
 });
