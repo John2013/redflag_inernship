c406095cfb787a5398f08240a0cf7c4c3f03b2fb
Evgeny Timofeyev <nawasington@ya.ru>
2018-06-20 14:06:09 +0300

    js 2d_arrays hard 4

--- 3_js/5_2d_arrays/3_hard/4/script.js
+++ 3_js/5_2d_arrays/3_hard/4/script.js
@@ -2,6 +2,23 @@ const app = getById('app');
 const button = getById('button');
 
 
-button.addEventListener('click', () => {
+const matrixOrderByFirstRowDesc = array => array.sort((a, b) => {
+	if (a[0] < b[0]) return 1;
+	if (b[0] < a[0]) return -1;
+	return 0
+});
 
+
+button.addEventListener('click', () => {
+	const n = getNumById('n');
+
+	const matrix = randIntMatrix(n, n, 0, 9);
+
+	const matrixTable = getTable(matrix);
+
+	const sortedMatrix = rotateMatrix(matrixOrderByFirstRowDesc(rotateMatrix(matrix)));
+
+	const sortedMatrixTable = getTable(sortedMatrix);
+
+	app.innerHTML = `${matrixTable}<br>${sortedMatrixTable}`
 });
