d7b33e0c6af20861ce41c8dd9ef3d2618b4ab7dc
Evgeny Timofeyev <nawasington@ya.ru>
2018-06-20 12:17:20 +0300

    js 2d_arrays hard 3

--- 3_js/5_2d_arrays/3_hard/3/script.js
+++ 3_js/5_2d_arrays/3_hard/3/script.js
@@ -2,6 +2,42 @@ const app = getById('app');
 const button = getById('button');
 
 
-button.addEventListener('click', () => {
+const getBottomFromSlaveMatrixDiag = matrix => {
+	for (let i in matrix){
+		for (let j in matrix[i]) {
+			if (i <= matrix.length - 1 - j){
+				matrix[i][j] = '';
+			}
+		}
+	}
 
+	return matrix
+};
+
+function isNumber(n) {
+	return !isNaN(parseFloat(n)) && isFinite(n);
+}
+
+const matrixMin = matrix =>
+	matrixReduce(
+		matrix,
+		(min, value) => (value < min && isNumber(value))? value: min,
+		matrix[1][matrix.length - 1]
+	);
+
+
+button.addEventListener('click', () => {
+	const n = getNumById('n');
+
+	const matrix = randIntMatrix(n, n, 0, 9);
+
+	const matrixTable = getTable(matrix);
+
+	const bottomFromDiag = getBottomFromSlaveMatrixDiag(matrix);
+
+	const bottomFromDiagTable = getTable(matrix);
+
+	const min = matrixMin(bottomFromDiag);
+
+	app.innerHTML = `${matrixTable}<br>${bottomFromDiagTable}<br>Минимальный ниже диагонали: ${min}`
 });
