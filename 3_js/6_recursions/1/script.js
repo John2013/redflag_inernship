53160084c27c3592dc2d530044e232ddc7c8f408
Evgeny Timofeyev <nawasington@ya.ru>
2018-06-21 17:32:32 +0300

    Добавил возможность использовать отрицательный параметр

--- 3_js/6_recursions/1/script.js
+++ 3_js/6_recursions/1/script.js
@@ -5,8 +5,12 @@ const range = (n) => {
 	if (typeof n !== 'object')
 		return range([n]);
 
-	if (n[0] > 1)
-		return range([n[0] - 1].concat(n));
+	if (n[0] > 0)
+		if (n[0] > 1)
+			return range([n[0] - 1].concat(n));
+
+		else if (n[n.length - 1] < 1)
+			return range(n.concat([(n[n.length - 1] + 1)]));
 
 	return n
 };
