de31f5cd93c0647fd785bb5260505757ab9d7356
Evgeny Timofeyev <nawasington@ya.ru>
2018-06-21 17:36:24 +0300

    recursions 5

--- 3_js/6_recursions/5/script.js
+++ 3_js/6_recursions/5/script.js
@@ -1,10 +1,21 @@
 const app = getById('app');
 const button = getById('button');
 
+const tensOf = n => Math.floor(n / 10);
+
+const sumOfDigits = (n, sum=0) => {
+	sum += 1;
+
+	if (n < 10)
+		return sum;
+
+	return sumOfDigits(tensOf(n), sum)
+};
+
 
 button.addEventListener('click', () => {
 
 	const n = getNumById('n');
 
-	app.innerHTML = ``
+	app.innerHTML = `${sumOfDigits(n)}`
 });
