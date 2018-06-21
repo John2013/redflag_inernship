21a168b6ffbfe7b1b675d2721f4e6792e282f29d
Evgeny Timofeyev <nawasington@ya.ru>
2018-06-21 11:09:35 +0300

    js recursions 3

--- 3_js/6_recursions/3/script.js
+++ 3_js/6_recursions/3/script.js
@@ -1,10 +1,24 @@
 const app = getById('app');
 const button = getById('button');
 
+const a = (m, n) => {
+	if (m === 0)
+		return n + 1;
+
+	if (n === 0){
+		return a(m - 1, 1)
+	}
+	else {
+		return a(m - 1, a(m, n - 1))
+	}
+};
+
 
 button.addEventListener('click', () => {
-
+	const m = getNumById('m');
 	const n = getNumById('n');
 
-	app.innerHTML = ``
+
+
+	app.innerHTML = `${a(m,n)}`
 });
