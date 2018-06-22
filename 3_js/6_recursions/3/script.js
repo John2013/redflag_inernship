6c0ea227174975c913e9264d55fb71aba644caea
Evgeny Timofeyev <nawasington@ya.ru>
2018-06-22 10:23:38 +0300

    recursions 3

--- 3_js/6_recursions/3/script.js
+++ 3_js/6_recursions/3/script.js
@@ -5,12 +5,10 @@ const a = (m, n) => {
 	if (m === 0)
 		return n + 1;
 
-	if (n === 0){
-		return a(m - 1, 1)
-	}
-	else {
-		return a(m - 1, a(m, n - 1))
-	}
+	if (n === 0)
+		return a(m - 1, 1);
+
+	return a(m - 1, a(m, n - 1))
 };
 
 
@@ -19,6 +17,5 @@ button.addEventListener('click', () => {
 	const n = getNumById('n');
 
 
-
-	app.innerHTML = `${a(m,n)}`
+	app.innerHTML = `${a(m, n)}`
 });
