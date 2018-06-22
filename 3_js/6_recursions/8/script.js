bbd639ab03746896f70dc28eb077788df9428232
Evgeny Timofeyev <nawasington@ya.ru>
2018-06-22 17:13:29 +0300

    недоделано

--- 3_js/6_recursions/8/script.js
+++ 3_js/6_recursions/8/script.js
@@ -2,6 +2,11 @@ const app = getById('app');
 const button = getById('button');
 
 
+const isSimple = (n, divider = 0) => {
+
+};
+
+
 button.addEventListener('click', () => {
 
 	const n = getNumById('n');
