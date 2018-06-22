fa013ce7d43f8c2761dd1d002ca9fd23d8bead38
Evgeny Timofeyev <nawasington@ya.ru>
2018-06-22 10:36:44 +0300

    recursions 6

--- 3_js/6_recursions/6/script.js
+++ 3_js/6_recursions/6/script.js
@@ -2,9 +2,17 @@ const app = getById('app');
 const button = getById('button');
 
 
+const printDigitsDesc = n => {
+	app.innerHTML += n % 10 + ' ';
+	if (n < 10) return true;
+
+	printDigitsDesc(Math.floor(n / 10))
+};
+
+
 button.addEventListener('click', () => {
 
 	const n = getNumById('n');
-
-	app.innerHTML = ``
+	app.innerHTML = '';
+	printDigitsDesc(n)
 });
