846aad24eed02e930ed0eeaa40f6b16360d1750f
Evgeny Timofeyev <nawasington@ya.ru>
2018-07-04 17:23:08 +0300

    5 2 2 5

--- 5_php/2_basics_practice/2_scripts_practice/index.php
+++ 5_php/2_basics_practice/2_scripts_practice/index.php
@@ -21,11 +21,8 @@ echo MarkdownExtra::defaultTransform(file_get_contents('./README.md'));
 		<?
 		$current_date = getdate();
 		$new_year_date = mktime(0, 0, 0, 1, 1, $current_date['year'] + 1);
-		$seconds_to_new_year = $new_year_date - time();
-		$seconds_in_year = 86400;
-		$days_to_new_year = (int)ceil($seconds_to_new_year / $seconds_in_year);
 		?>
-		<p><?= var_export($days_to_new_year) ?></p>
+		<p><?= days_to_time($new_year_date) ?></p>
 	</li>
 	<li>
 		<p>Дан инпут и кнопка. В этот инпут вводится год. По нажатию на кнопку выведите на экран, високосный он или
@@ -50,16 +47,25 @@ echo MarkdownExtra::defaultTransform(file_get_contents('./README.md'));
 		<p>Дан инпут и кнопка. В этот инпут вводится дата в формате '01.12.1990'. По нажатию на кнопку выведите на экран
 			день недели, соответствующий этой дате, например, 'воскресенье'.</p>
 		<?
-		function weeks_day_to_string($int_day){
-			switch ($int_day){
-				case 0: return 'воскресенье';
-				case 1: return 'понедельник';
-				case 2: return 'вторник';
-				case 3: return 'среда';
-				case 4: return 'четверг';
-				case 5: return 'пятница';
-				case 6: return 'суббота';
-				default: return false;
+		function weeks_day_to_string($int_day)
+		{
+			switch ($int_day) {
+				case 0:
+					return 'воскресенье';
+				case 1:
+					return 'понедельник';
+				case 2:
+					return 'вторник';
+				case 3:
+					return 'среда';
+				case 4:
+					return 'четверг';
+				case 5:
+					return 'пятница';
+				case 6:
+					return 'суббота';
+				default:
+					return false;
 			}
 		}
 
@@ -80,8 +86,39 @@ echo MarkdownExtra::defaultTransform(file_get_contents('./README.md'));
 		<p><?= date('d F Y года, l') ?></p>
 	</li>
 	<li>
-		<p>Дан инпут и кнопка. В этот инпут вводится дата рождения в формате '01.12.1990'. По нажатию на кнопку выведите на экран сколько дней осталось до дня рождения пользователя.</p>
-		<p></p>
+		<p>Дан инпут и кнопка. В этот инпут вводится дата рождения в формате '01.12.1990'. По нажатию на кнопку выведите
+			на экран сколько дней осталось до дня рождения пользователя.</p>
+		<?
+		$date_str = htmlspecialchars($_REQUEST['date3']) ?: date('Y-m-d');
+		$date_int = strtotime($date_str);
+		?>
+		<form action="#birth_year" method="post">
+			<label for="birth_year">Дата рождения</label>
+			<input type="date" name="birth_year" id="birth_year" max="<?= date('Y-m-d') ?>" value="<?= $date_str ?>">
+			<input type="submit">
+		</form>
+		<?
+		$birth_date = getdate($date_int);
+		$next_birthday = mktime(
+			0,
+			0,
+			0,
+			$birth_date["mon"],
+			$birth_date["mday"],
+			(int)date('Y')
+		);
+		if ($next_birthday < time()) {
+			$next_birthday = mktime(
+				0,
+				0,
+				0,
+				$birth_date["mon"],
+				$birth_date["mday"],
+				(int)date('Y') + 1
+			);
+		}
+		?>
+		<p><?= days_to_time($next_birthday) ?></p>
 	</li>
 	<li>
 		<p></p>
--- 5_php/base.php
+++ 5_php/base.php
@@ -1,2 +1,11 @@
 <?
 require $_SERVER['DOCUMENT_ROOT'].'/vendor/autoload.php';
+
+function days_to_time($time, $cur_time = null){
+	if(!$cur_time){
+		$cur_time = time();
+	}
+	$seconds_to_time = $time - $cur_time;
+	$seconds_in_year = 86400;
+	return (int)ceil($seconds_to_time / $seconds_in_year);
+}
