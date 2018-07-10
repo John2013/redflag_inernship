f1840cf0ae4d952021d4829ed769738f01c6deb8
Evgeny Timofeyev <nawasington@ya.ru>
2018-07-10 16:13:56 +0300

    db5-6

--- 5_php/4_db_practice/1_basics/index.php
+++ 5_php/4_db_practice/1_basics/index.php
@@ -7,6 +7,10 @@ function map_htmlspecialchars($item)
 {
 	return htmlspecialchars($item);
 }
+function map_filter($item)
+{
+	return $item ? htmlspecialchars($item) : null;
+}
 
 function table_get_by_id($table_array, $id)
 {
@@ -28,6 +32,20 @@ function table_add_col_link($table_array, $col_title, $get_param_name, $get_para
 	return $new_array;
 }
 
+function table_add_col_checkbox($table_array, $col_title, $get_param_name, $get_param_value_col, $checkbox_title,
+                                $form_id, $checked_array = [])
+{
+	$new_array = [];
+	foreach ($table_array as $r_number => $row) {
+		$name = $get_param_name . "[" . $row[$get_param_value_col] . "]";
+		$is_checked = $checked_array[$row[$get_param_value_col]] ? " checked" : "";
+		$row[$col_title] = "<input type='checkbox' name='$name' title='$checkbox_title' form='$form_id'$is_checked>";
+		$new_array[] = $row;
+	}
+
+	return $new_array;
+}
+
 if ($_GET['del_id']) {
 	pg_delete($dbconn, 'workers', ['id' => (int)$_GET['del_id']]);
 }
@@ -43,6 +61,17 @@ if ($_REQUEST['db4_submit']) {
 	$res = pg_update($dbconn, 'workers', $change_array, ['id' => $id]);
 }
 
+if($_REQUEST['db6_del']) {
+	$delete_array = [];
+	foreach ($_REQUEST['db6_del'] as $id => $_){
+		$delete_array[] = (int)$id;
+	}
+	$id_list = implode(', ', $delete_array);
+	$query = "DELETE FROM workers WHERE id in ($id_list)";
+	pprint($query);
+	pg_query($dbconn, $query);
+}
+
 $rs = pg_query($dbconn, 'SELECT * FROM workers');
 $table_array = pg_fetch_all($rs);
 
@@ -51,6 +80,12 @@ $table_array = pg_fetch_all($rs);
 <head>
 	<meta charset="UTF-8">
 	<title></title>
+	<script>
+		function filter_keypress(e) {
+			if (e.charCode === 13)
+				e.target.form.submit()
+		}
+	</script>
 </head>
 <body>
 <?
@@ -80,11 +115,12 @@ echo MarkdownExtra::defaultTransform(file_get_contents('./README.md'));
 				<label for="name">name</label>
 			</div>
 			<div>
-				<input type="text" id="age" name="add[age]" value="<?= $_REQUEST['add']['age'] ?>">
+				<input type="number" id="age" name="add[age]" value="<?= $_REQUEST['add']['age'] ?>">
 				<label for="age">age</label>
 			</div>
 			<div>
-				<input type="text" id="salary" name="add[salary]" value="<?= $_REQUEST['add']['salary'] ?>">
+				<input type="number" id="salary" name="add[salary]" step="100"
+				       value="<?= $_REQUEST['add']['salary'] ?>">
 				<label for="salary">salary</label>
 			</div>
 			<input type="submit" name="db3_submit">
@@ -102,7 +138,7 @@ echo MarkdownExtra::defaultTransform(file_get_contents('./README.md'));
 		if ($_REQUEST['change_id']) {
 			$id = (int)$_REQUEST['change_id'];
 			?>
-			<form action="#db4">
+			<form action="#db4" method="post">
 				<input type="hidden" name="change[id]" value="<?= $id ?>">
 				<div>
 					<input type="text" id="name" name="change[name]"
@@ -110,12 +146,12 @@ echo MarkdownExtra::defaultTransform(file_get_contents('./README.md'));
 					<label for="name">name</label>
 				</div>
 				<div>
-					<input type="text" id="age" name="change[age]"
+					<input type="number" id="age" name="change[age]"
 					       value="<?= table_get_by_id($table_array, $id)['age'] ?>">
 					<label for="age">age</label>
 				</div>
 				<div>
-					<input type="text" id="salary" name="change[salary]"
+					<input type="number" id="salary" name="change[salary]" step="100"
 					       value="<?= table_get_by_id($table_array, $id)['salary'] ?>">
 					<label for="salary">salary</label>
 				</div>
@@ -125,12 +161,46 @@ echo MarkdownExtra::defaultTransform(file_get_contents('./README.md'));
 		}
 		?>
 	</li>
-	<li>
+	<li id="db5">
 		<p>Над таблицей с работниками сделайте инпут, в который вводится зарплата. По нажатию на кнопку следует вывести
 			таблицу работников с введенной зарплатой.</p>
+		<form action="./#db5" id="filter_form"></form>
+		<?
+		$filter = array_map("map_htmlspecialchars", $_REQUEST['filter']);
+		foreach ($filter as $key => $item) {
+			if (!$item)
+				unset($filter[$key]);
+		}
+		$filter_row = "<tr>
+<td></td>
+<td><input type='text' name='filter[name]' form='filter_form' value='{$filter['name']}' onkeypress='return filter_keypress(event)'></td>
+<td><input type='number' name='filter[age]' form='filter_form' value='{$filter['age']}' onkeypress='return filter_keypress(event)'></td>
+<td><input type='number' name='filter[salary]' form='filter_form' value='{$filter['salary']}' step='100' onkeypress='return filter_keypress(event)'></td>
+</tr>";
+
+		$table_array5 = $filter
+			? pg_select($dbconn, 'workers', $filter)
+			: $table_array;
+
+		echo get_html_table($table_array5, $filter_row);
+		?>
 	</li>
-	<li></li>
-	<li></li>
+	<li id="db6">
+		<p>Сделайте колонку 'удалить', в которой для каждого работника будет стоять чекбокс. Под таблицей сделайте
+			кнопку, по нажатию на которую будут удалены те работники, для которых чекбокс был отмечен.</p>
+		<form action="./#db6" method="post" id="db6_form"></form>
+		<?
+		$delete_array = array_map("map_htmlspecialchars", $_REQUEST['db6_del']);
+
+		$table_array6 = table_add_col_checkbox($table_array,'удалить', 'db6_del',
+			'id', 'удалить', 'db6_form', $delete_array);
+
+		echo get_html_table($table_array6);
+
+
+		?>
+		<input type="submit" value="удалить" form="db6_form">
+	</li>
 </ol>
 </body>
 </html>
