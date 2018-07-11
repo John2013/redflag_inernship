73ace7f20568ef69c642ead7a8364e60ca709ad4
Evgeny Timofeyev <nawasington@ya.ru>
2018-07-11 16:47:45 +0300

    5 4 2 3 модель

---
+++ 5_php/4_db_practice/2_miniprojects/Note.php
@@ -0,0 +1,97 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: evgeny
+ * Date: 11.07.2018
+ * Time: 11:04
+ */
+
+class GuestNote
+{
+	public $id;
+	public $title;
+	public $text;
+	public $time;
+	const TABLE_NAME = 'notes';
+
+	function __construct($title, $text, $time = null, $id = null)
+	{
+		$this->title = $title;
+		$this->text = $text;
+		$this->time = $time ?: time();
+		$this->id = $id;
+	}
+
+	function __toString()
+	{
+		return $this->text;
+	}
+
+	function get_time()
+	{
+		return date('d.m.Y H:i:s', $this->time);
+	}
+
+	function get_assoc()
+	{
+		return [
+			'id' => $this->id,
+			'created_at' => $this->time,
+			'text' => $this->text,
+			'title' => $this->title
+		];
+	}
+
+	function create()
+	{
+		$table_name = self::TABLE_NAME;
+		$query = "INSERT INTO $table_name(name,text,created_at) 
+VALUES ('{$this->title}','{$this->text}',{$this->time}) 
+RETURNING id";
+		$rs = pg_query(DBCONN, $query);
+		$this->id = pg_fetch_array($rs)[0];
+		return $this->id;
+	}
+
+	function update()
+	{
+		$data = $this->get_assoc();
+		$condition = ['id' => $this->id];
+
+		return pg_update(DBCONN, self::TABLE_NAME, $data, $condition);
+	}
+
+	function save()
+	{
+		return isset($this->id) ? $this->update() : $this->create();
+	}
+
+	static function load($id=null, $page=null, $page_size=3)
+	{
+		if ($id !== null){
+			$assoc_array = pg_select(DBCONN, self::TABLE_NAME, ['id' => $id])[0];
+
+			return new self($assoc_array['title'], $assoc_array['text'], $assoc_array['created_at'], $assoc_array['id']);
+		}
+		else{
+			$query = "SELECT * FROM " . self::TABLE_NAME;
+			if(isset($page)){
+				$query .= " LIMIT " . $page_size . " OFFSET " . $page * $page_size;
+			}
+			$rs = pg_query(DBCONN, $query);
+			$assoc_array = pg_fetch_all($rs);
+			$objects = [];
+			foreach ($assoc_array as $row){
+				$objects[] = new self($row['title'], $row['text'], $row['created_at'], $row['id']);
+			}
+			return $objects;
+		}
+
+	}
+
+	static function get_count(){
+		$query = "SELECT COUNT(id) FROM " . self::TABLE_NAME;
+		$rs = pg_query(DBCONN, $query);
+		return pg_fetch_array($rs)[0];
+	}
+}
