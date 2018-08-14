68e54b98fb2c0eb1e94c102d14d97fa212c52c35
Evgeny Timofeyev <nawasington@ya.ru>
2018-08-14 12:54:24 +0300

    ...

---
+++ 7_yii2/console/controllers/AgentsController.php
@@ -0,0 +1,22 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: evgeny
+ * Date: 14.08.2018
+ * Time: 12:07
+ */
+
+namespace console\controllers;
+
+
+use backend\models\Session;
+use yii\console\ExitCode;
+
+class AgentsController extends \yii\console\Controller
+{
+	function actionClearOldSessions(){
+		Session::deleteNonActual();
+
+		return ExitCode::OK;
+	}
+}
